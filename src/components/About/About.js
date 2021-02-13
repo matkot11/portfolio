bf7f928ac335a5d4810091fbd47a77ef6239b346
matkot11 <matkot11@gmail.com>
2021-02-13 18:21:54 +0000

    Deleted my photo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import Image from 'gatsby-image';
 import styled from 'styled-components';
 import BackgroundImage from '../../assets/images/backgroundCoffee.jpg';
 
@@ -26,14 +24,10 @@ const StyledWrapper = styled.div`
 const StyledInnerWrapper = styled.div`
   width: calc(100vw - 16rem);
   display: flex;
-  justify-content: space-evenly;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   padding: 0 2rem;
-
-  @media only screen and (max-width: ${({ theme }) => theme.size.xl}) {
-    flex-direction: column;
-    justify-content: center;
-  }
 `;
 
 const StyledTitle = styled.h2`
@@ -53,46 +47,21 @@ const StyledParagraph = styled.p`
   }
 `;
 
-const StyledImage = styled(Image)`
-  width: 50rem;
+const About = () => (
+  <StyledWrapper id="about">
+    <StyledInnerWrapper>
+      <StyledTitle>A little bit about me</StyledTitle>
+      <StyledParagraph>
+        I code for as long as I can remember. My journey with coding started
+        with Arduino and when I realised I love coding I have started looking
+        what best fits me and as a result I ended up with Frontend. I am
+        learning that for almost two years and still love it. That&apos;s why I
+        went to England to study Computer Science in order to further my
+        knowledge in coding. I am currently in my first year and besides my
+        studies I would like to get experience in work.
+      </StyledParagraph>
+    </StyledInnerWrapper>
+  </StyledWrapper>
+);
 
-  @media only screen and (max-width: ${({ theme }) => theme.size.xl}) {
-    margin-bottom: 5rem;
-    display: none;
-  }
-`;
-
-const About = () => {
-  const data = useStaticQuery(graphql`
-    {
-      file(name: { eq: "face" }) {
-        childImageSharp {
-          fluid(quality: 100) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-    }
-  `);
-
-  return (
-    <StyledWrapper id="about">
-      <StyledInnerWrapper>
-        <div>
-          <StyledTitle>A little bit about me</StyledTitle>
-          <StyledParagraph>
-            I code for as long as I can remember. My journey with coding started
-            with Arduino and when I realised I love coding I have started
-            looking what best fits me and as a result I ended up with Frontend.
-            I am learning that for almost two years and still love it.
-            That&apos;s why I went to England to study computer science in order
-            to further my knowledge in coding. I am currently in my first year
-            and besides my studies I would like to get experience in work.
-          </StyledParagraph>
-        </div>
-        <StyledImage fluid={data.file.childImageSharp.fluid} alt="face" />
-      </StyledInnerWrapper>
-    </StyledWrapper>
-  );
-};
 export default About;

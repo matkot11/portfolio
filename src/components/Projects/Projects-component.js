b0513838b52fe399003ff56ca306326634c01f75
matkot11 <matkot11@gmail.com>
2021-07-13 10:54:13 +0100

    Added sort method to projects

--- src/components/Projects/Projects-component.js
+++ src/components/Projects/Projects-component.js
@@ -18,13 +18,14 @@ const Projects = () => {
     {
       allDatoCmsProject {
         nodes {
+          myid
+          title
+          paragraph
           image {
             fluid(maxWidth: 700) {
               ...GatsbyDatoCmsFluid_tracedSVG
             }
           }
-          title
-          paragraph
           technologies {
             svg {
               url
@@ -40,33 +41,35 @@ const Projects = () => {
   return (
     <>
       <StyledTitle id="projects">My projects</StyledTitle>
-      {data.allDatoCmsProject.nodes.map((item) => (
-        <StyledWrapper key={item.title}>
-          <StyledInnerWrapper>
-            <StyledProjectName>{item.title}</StyledProjectName>
-            <StyledParagraph>{item.paragraph}</StyledParagraph>
-            <StyledRowWrapper>
-              {item.technologies.map((svg) => (
-                <StyledSVGWrapper key={svg.svg.url}>
-                  <StyledSVG src={svg.svg.url} alt={svg.svgName} />
-                  <span>{svg.svgName}</span>
-                </StyledSVGWrapper>
-              ))}
-            </StyledRowWrapper>
-            <>
-              <StyledLink href={item.pageLink} target="_blank">
-                Link to page
-              </StyledLink>
-              <StyledLink href={item.githubLink} target="_blank">
-                Link to Github
-              </StyledLink>
-            </>
-          </StyledInnerWrapper>
-          <a href={item.pageLink} target="_blank">
-            <StyledImage fluid={item.image.fluid} alt="project" />
-          </a>
-        </StyledWrapper>
-      ))}
+      {data.allDatoCmsProject.nodes
+        .sort((a, b) => (a.myid > b.myid ? 1 : -1))
+        .map((item) => (
+          <StyledWrapper key={item.title}>
+            <StyledInnerWrapper>
+              <StyledProjectName>{item.title}</StyledProjectName>
+              <StyledParagraph>{item.paragraph}</StyledParagraph>
+              <StyledRowWrapper>
+                {item.technologies.map((svg) => (
+                  <StyledSVGWrapper key={svg.svg.url}>
+                    <StyledSVG src={svg.svg.url} alt={svg.svgName} />
+                    <span>{svg.svgName}</span>
+                  </StyledSVGWrapper>
+                ))}
+              </StyledRowWrapper>
+              <>
+                <StyledLink href={item.pageLink} target="_blank">
+                  Link to page
+                </StyledLink>
+                <StyledLink href={item.githubLink} target="_blank">
+                  Link to Github
+                </StyledLink>
+              </>
+            </StyledInnerWrapper>
+            <a href={item.pageLink} target="_blank">
+              <StyledImage fluid={item.image.fluid} alt="project" />
+            </a>
+          </StyledWrapper>
+        ))}
     </>
   );
 };

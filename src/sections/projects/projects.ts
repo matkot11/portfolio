cae6a1853422787d8d7fcd4c94fd19baa3a439e9
Mateusz Kocik <matkot11@gmail.com>
2025-03-31 12:35:46 +0200

    refactor: Update projects about

--- src/sections/projects/projects.ts
+++ src/sections/projects/projects.ts
@@ -5,7 +5,7 @@ export const projects = [
     website: 'https://rickandmortychat.netlify.app',
     repository: 'https://github.com/matkot11/chat-app',
     about:
-      'The Rick and Morty Chat app is a fun and interactive web application that allows users to search through a database of Rick and Morty characters, authenticate  to access personalized features, send messages to their favorite characters and receive responses from those characters. Creating this project improved my skills in effective consumption of REST APIs to fetch and display dynamic content from the Rick and Morty API. Managing chat data and user sessions enhanced my experience with Supabase features. Creating design with Shadcn improved my skills in  using third party design system by finding it limits and how to adjust external library to my use cases.',
+      'The Rick and Morty Chat app is a fun and interactive web application that allows users to search through a database of Rick and Morty characters, authenticate to access personalized features, send messages to their favorite characters and receive responses from those characters. Creating this project improved my skills in effective consumption of REST APIs to fetch and display dynamic content from the Rick and Morty API. Managing chat data and user sessions enhanced my experience with Supabase features. Creating design with Shadcn improved my skills in using third party design system by finding it limits and how to adjust external library to my use cases.',
     techStack: [
       {
         name: 'React',
@@ -47,7 +47,7 @@ export const projects = [
     website: 'https://pogadajmyoagile.pl',
     repository: 'https://github.com/matkot11/pogadajmy_o_agile',
     about:
-      'The Pogadajmy o Agile website was developed for a client and serves as a dedicated platform for an Agile-focused podcast. The website introduces the podcast hosts and showcases the latest episodes by integrating with the Spotify API. This feature allows new content to be displayed automatically, ensuring visitors always have access to the most recent episodes. Building this website expanded my knowledge in communication with client and designer, by defining clients needs and cooperating with designer. By multiple meetings with the client I was able to create the website that client is satisfied.',
+      'The Pogadajmy o Agile website was developed for a client and serves as a dedicated platform for an Agile-focused podcast. The website introduces the podcast hosts and showcases the latest episodes by integrating with the Spotify API. This feature allows new content to be displayed automatically, ensuring visitors always have access to the most recent episodes. Building this website strengthened my ability to communicate effectively with both the client and the designer. By thoroughly defining the client’s needs and collaborating closely with the designer, I ensured that the final product aligned with the client’s vision. Through multiple meetings and iterative feedback sessions, I was able to deliver a website that met the client’s expectations.',
     techStack: [
       {
         name: 'Vue',
